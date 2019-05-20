c035bb899c7da4c592cfc21f875038796f5a4e30
cherish <xieqin1991@gmail.com>
2019-05-20 14:43:13 +0800

    插入排序

--- sort/insert_sort.js
+++ sort/insert_sort.js
@@ -4,21 +4,19 @@
  * @param {array} array 
  */
 const insertSort = (array) => {
-  let temp = []
   if (array.length <= 1) {
     return array
   }
   for (let i = 0; i < array.length; i++ ) {
-    temp.push(array[i])
+    let temp = array[i]
     for (let j = i; j > 0; j --) {
-      if (temp[j] < temp[j - 1]) {
-        let t = temp[j]
-        temp[j] = temp[j - 1]
-        temp[j - 1] = t
+      if (array[j] < array[j - 1]) {
+        array[j] = array[j - 1]
+        array[j - 1] = temp
       }
     }
   }
-  return temp
+  return array
 }
 
 let data = [16, 4, 6, 0, 3, 11, 7, 23, 3, 28, 78, 9, 71, 12]
