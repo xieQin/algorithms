c9f6ec492295b7b5ec02b2f964c1dba01092c07f
cherish <xieqin1991@gmail.com>
2019-06-17 16:09:51 +0800

    算法习题39

--- exercises/ex39.js
+++ exercises/ex39.js
@@ -8,6 +8,8 @@ const swap = (A, p, q) => {
   A[q] = temp
 }
 
+// 基于partition的时间复杂度为O(n)算法
+// 数组中有一个数字出现次数超过数组长度的一半，则数组排序后该数字一定出现在中位数上
 const findNumer = A => {
   let start = 0, end = A.length - 1
   let middle = Math.floor((start + end) / 2), r = partition(A, start, end)
@@ -38,4 +40,23 @@ const partition = (A, p, q) => {
   return i
 }
 
+// 顺序查找, O(n)
+const findNumer2 = A => {
+  let number = A[0]
+  let count = 1
+  for (let i = 1; i < A.length; i ++) {
+    if (number == A[i]) {
+      count ++
+    } else {
+      count --
+      if (count <= 0) {
+        number = A[i]
+        count = 1
+      }
+    }
+  }
+  return number
+}
+
 console.log(findNumer([1,2,3,2,5,4,2,3,3,3,3,3,3,3,3,3,3,6,7,8,3,3,8,3]))
+console.log(findNumer2([1,2,3,2,5,4,2,3,3,3,3,3,3,3,3,3,3,6,7,8,3,3,8,3]))
