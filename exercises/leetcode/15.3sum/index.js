d7115d1f6fb41a5a601309ef4802650e406c626f
cherish <xieqin1991@gmail.com>
2019-07-17 11:31:09 +0800

    增加leetcode习题15、16、17

--- exercises/leetcode/15.3sum/index.js
+++ exercises/leetcode/15.3sum/index.js
@@ -9,14 +9,30 @@ var threeSum = function (nums) {
   if (nums.length <= 2) {
     return []
   }
-  let sumResult = []
+  let sumResult = new Set()
   for (let i = 0; i < nums.length; i ++) {
     let target = 0 - nums[i],j = 0
     while (j < nums.length) {
+      let r = nums.indexOf(target - nums[j])
+      if (j != i && r > -1 && r != i) {
+        let res = [nums[i], nums[j], nums[r]].sort((a, b) => a - b)
+        if (!sumResult.has(res)) {
+          sumResult.add(res)
+        }
+      }
       j ++
     }
   }
+  return sumResult
 };
+
+const a = new Set()
+
+a.add([1, 2])
+a.add([1,2])
+a.add(1)
+
+console.log(a)
 
 const nums = [-1, 0, 1, 2, -1, -4]
 
